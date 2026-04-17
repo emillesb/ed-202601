991ac7dee12432fe099697e4784da20254d8a1ac
ZombieShark <emillesbdev@gmail.com>
2026-04-17 11:14:05 -0300

    adiciona aula04

---
+++ aulas/aula04/selecao.h
@@ -0,0 +1,10 @@
+#ifndef SELECAO_H
+#define SELECAO_H
+
+long long int busca_sequencial(long long int v[], long long int n, long long int valor);
+int quick_select(int v[], int inicio, int fim);
+
+
+
+
+#endif
